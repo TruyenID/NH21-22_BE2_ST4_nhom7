1d47c3e327ebdeb69399bfe8a59d86eb04739f6b
TranDuc41 <td07197@gmail.com>
2022-05-04 09:16:57 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\MyController;
 
 /*
@@ -14,61 +14,13 @@ use App\Http\Controllers\MyController;
 |
 */
 Route::get('/{name?}',[MyController::class, 'index']);
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/test', function () {
-//     return view('test');
-// });
-// Route::get('/EXE', function () {
-//     return view('index');
-// });
-// Route::get('/trangchu', function () {
-//     return view('trangchu');
-// });
-// Route::get('/gioithieu', function () {
-//     return view('gioithieu');
-// });
-// Route::get('/lienhe', function () {
-//     return view('lienhe');
-// });
-// Route::get('/ban', function () {
-//     return view('ban');
-// });
-// Route::get('/ghe', function () {
-//     return view('ghe');
-// });
-// Route::get('/{name?}', function ($name= "trangchu") {
-//     //echo "Xin chao ".$name;
-//     return view($name);
-// });
-// Route::get('rp/{name}', function ($name) {
-//     echo "Xin chao ".$name;
-// })->where('name', '[A-Za-z]+');
-// Route::get('op/{name}', function ($name="demo") {
-//     echo "Xin chao ".$name;
-// });
 
-// Route::get('/user/{id}/{name}', function ($id, $name) {
-//     echo "Xin chao ".$id.$name;  
-// })->where('id', '[0-9]+')->where('name', '[A-Za-z]+');;; 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/user/{name}', function ($name) {
-//     echo "Xin chao ".$name;
-// })->whereAlphaNumeric('name');
- 
-// Route::get('/user/{id}', function ($id) {
-//     echo "Xin chao ".$id;
-// })->where('id', '[0-9]+');
-// Route::get('/{name?}', function ($name="index") {
-//     return view($name);
-// });
-// Route::resourse('/product',ProductController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Route::get('/product',[ProductController::Class,'index']);
-// Route::get('/product/create',[ProductController::Class,'create']);
-// Route::post('/product',[ProductController::Class,'store']);
-// Route::get('/product/{id}',[ProductController::Class,'show']);
-// Route::get('/product/{id}/edit',[ProductController::Class,'edit']);
-// Route::patch('/product/{id}',[ProductController::Class,'update']);
-// Route::delete('/product/{id}',[ProductController::Class,'destroy']);
+require __DIR__.'/auth.php';
